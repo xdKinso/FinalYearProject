d6f3ee689b4e13d72897dab839d6fc2a5a74409b
xdKinso <124276188+xdKinso@users.noreply.github.com>
2023-12-03 20:03:19 +0000

    My project base

---
+++ flask-react-app/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, jsonify
+import time
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/api')
+def api():
+    return jsonify(message="Hello from Flask!")
+
+@app.route('/time')
+def get_current_time():
+    return {'time': time.time()}
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
