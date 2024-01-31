bfd01382f68cc5c69994cd2772dbc6f1497a9dc0
kinso <mowinskikrystian@gmail.com>
2024-01-31 20:05:36 +0000

    Changes to about page

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -6,6 +6,13 @@ class About extends React.Component {
     return (
       <div>
         <h1>About Page</h1>
+        <p>The purpose of this Website is to have a simple way to store and interact with your Games and their statistics. This tool Is mainly used for improvement
+          and to track your progress over time. This is a personal project that I am working on and I am open to suggestions and feedback. I am currently working on
+          a way to store your data on a database so that you can login and view your stats from anywhere. I am also working on a way to add more games and their
+          statistics. I am also working on a way to add a graph to the profile page so that you can see your progress over time. I am also working on a way to add
+          friends
+
+        </p>
       </div>
     );
   }
